9f1bf073dd0e6657e1bf94adab9d5a15a8d768fa
Trish Daugherty <99498083+Trish2022@users.noreply.github.com>
2022-04-27 16:54:25 -0700

    Update script.js
    removed some comments

--- js/script.js
+++ js/script.js
@@ -9,18 +9,16 @@ const addEmployee = (id, name, ext, email, dept) => {
         email: email,
         dept: dept
     });
-    localStorage.setItem("employees", JSON.stringify(employeesArray));
 
     return {id, name, ext, email, dept}
 }
  let employeesArray = JSON.parse(localStorage.getItem('employees')) || [];
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
- if (!(localStorage.getItem("employeeItem") == true || localStorage.getItem("employeeItem") == false)) {
+ if (!(localStorage.getItem("employees") == true || localStorage.getItem("employees") == false)) {
     // init variable/set default variable for item
-    localStorage.setItem("employeeItem", true);
+     localStorage.setItem("employees", JSON.stringify(employeesArray));
 }
-
 
 // GET DOM ELEMENTS
 const $ = (id) => {
@@ -28,10 +26,9 @@ const $ = (id) => {
 };
 
 // // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-   let empForm=  document.getElementById('addForm')
-//    let formElement =  document.getElementById('addForm');
-   let tbodyElement = document.querySelector('tbody');
-   let empTable = document.querySelector('#employees');
+        let empForm         = document.getElementById('addForm');
+        let tbodyElement    = document.querySelector('tbody');
+        let empTable        = document.querySelector('#employees');
    
 // ADD EMPLOYEE
        function addEmployees(e) {  
@@ -40,23 +37,21 @@ const $ = (id) => {
 //     // PREVENT FORM SUBMISSION
        e.preventDefault();
 // GET THE VALUES FROM THE TEXT BOXES
-       let id =   document.getElementById('id').value;
-       let name = document.getElementById('name').value;
-       let extension = document.getElementById('extension').value;
-       let email =  document.getElementById('email').value;
-       let department =  document.getElementById('department').value;
+       let id           = document.getElementById('id').value;
+       let name         = document.getElementById('name').value;
+       let extension    = document.getElementById('extension').value;
+       let email        = document.getElementById('email').value;
+       let department   = document.getElementById('department').value;
 
  // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
-function addEmployee(employee) {
-    employees.push(employee);
-    localStorage.employees = JSON.stringify(employees);
+    function addEmployee(employee) {
+        employees.push(employee);
+        localStorage.employees = JSON.stringify(employees);
 }
  // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
  // BUILD THE GRID
-
        let empForm=  document.getElementById('addForm').value;
-
-       tbodyElement.innerHTML += `
+            tbodyElement.innerHTML += `
             <tr>
                 <td>${id}</td>
                 <td>${name}</td>
@@ -66,12 +61,11 @@ function addEmployee(employee) {
                 <td><button class = "deleteBtn">Delete</td>
             </tr>
       `;
- 
  };
 
 // DELETE EMPLOYEE
    function onDeleteRow(e) {
-       if(!e.target.classList.contains('deleteBtn')){
+      if(!e.target.classList.contains('deleteBtn')){
            return;
        }
      else if (confirm('Are you sure you want to delete this employee?')) {
@@ -82,44 +76,9 @@ function addEmployee(employee) {
    empForm.addEventListener('submit', addEmployees);
    empTable.addEventListener('click', onDeleteRow);
 
-
-
-
-
-
-//     // RESET THE FORM
-    addForm.reset();
+// RESET THE FORM
+    empForm.reset();
 // SET FOCUS BACK TO THE ID TEXT BOX
-    addForm.id.focus();
-
-
-
-// empTable.addEventListener('click', (e) => {
-//     // CONFIRM THE DELETE
-
-//         // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
-
-//         // CALL DELETEROW() METHOD TO DELETE SPECIFIC ROW IN THE TABLE
-
-//         // REMOVE EMPLOYEE FROM ARRAY
-
-//         // BUILD THE GRID
-
-// });
-
-// // BUILD THE EMPLOYEES GRID
-// function buildGrid() {
-//     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
-
-//     // Adding the entire table to the body tag
-//         document.getElementById('body').appendChild(table);
-//     // LOOP THROUGH THE ARRAY OF EMPLOYEES
-//     // REBUILDING THE ROW STRUCTURE
-
-
-//     // BIND THE TBODY TO THE EMPLOYEE TABLE
-
-
-//     // STORE THE ARRAY IN STORAGE
+    empForm.id.focus();
 
 // };
